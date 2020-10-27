af81bbf2f8933e23c3b502f1b59c316ebc491d3e
sydney miranda <sydmiranda@icloud.com>
2020-10-27 16:23:37 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
-function forLoop (array) {
+function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
       return "I am 1 strange loop."
@@ -8,7 +8,7 @@ function forLoop (array) {
   }
 }
 
-function whileLoop (n) {
+function whileLoop(n) {
   while (n > 0) {
     console.log(--n)
           return 'done'
