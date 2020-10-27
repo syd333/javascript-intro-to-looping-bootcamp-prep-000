8a84318bdc8d112b2398fea814bc15f0516b16be
sydney miranda <sydmiranda@icloud.com>
2020-10-27 16:24:40 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -14,13 +14,13 @@ function forLoop(array) {
 //           return 'done'
 //   }
 // }
-// 
-// 
+//
+//
 // function incrementVariable() {
 //   i = i + 1;
 //   return i;
 // }
-// 
+//
 // function doWhileLoop(num) {
 //   do {
 // console.log('I run once regardless.');
