5c42501ce0b962083d19f86c67578c82507d65ac
sydney miranda <sydmiranda@icloud.com>
2020-10-27 16:39:09 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -21,8 +21,8 @@ function incrementVariable() {
   i = i + 1;
   return i;
 }
-//
-// function doWhileLoop(num) {
-//   do {
-// console.log('I run once regardless.');
-// } while (incrementVariable() < num)
+
+function doWhileLoop(num) {
+  do {
+console.log('I run once regardless.');
+} while (incrementVariable() < num)
